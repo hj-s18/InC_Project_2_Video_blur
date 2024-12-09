51fec24a050df9b5bbacca850a4c72c06f9ce48a
ki <redcedar1@naver.com>
2024-12-09 14:58:06 +0900

    flask app hosting ip changed into anywhere from ipv4

--- app.py
+++ app.py
@@ -399,5 +399,5 @@ def index():
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
  
